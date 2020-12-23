8b25de0a295c0eb1f55173efba8ae27dac7a4b22
sushmithareddy10 <sushmithareddy1098@gmail.com>
2020-12-23 14:09:20 +0530

    Api error handling

--- src/actions/DoctorAction.js
+++ src/actions/DoctorAction.js
@@ -1,63 +1,70 @@
-import axios from 'axios';
-import {DELETE_DOCTOR, GET_DOCTOR, GET_DOCTORS, GET_ERRORS} from "./type";
-export const addDoctor = (doctor,history) => async (dispatch) => {
-    let res ={};
-    try {
-        res = await axios.post("http://localhost:8080/api/doctors/insert",doctor);
-        history.push("/get-doctor");
-    }catch(error) {
-        console.log("ERROR", error)
-       dispatch({
-        type: GET_ERRORS,
-        payload: error.response.data,
-       });
-    }
+import axios from "axios";
+import { DELETE_DOCTOR, GET_DOCTOR, GET_DOCTORS, GET_ERRORS } from "./type";
+export const addDoctor = (doctor, history) => async (dispatch) => {
+  let res = {};
+  try {
+    res = await axios.post("http://localhost:8080/api/doctors/insert", doctor);
+    history.push("/get-doctor");
+  } catch (error) {
+    console.log("ERROR", error);
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
 };
 
 export const getDoctors = () => async (dispatch) => {
-    const res = await axios.get("http://localhost:8080/api/doctors/all");
-    fetch(res).then(response => {
-        if(response.ok) {
-            return response;
-        }
-        else{
-            var error = new Error('Error' + response.status+ ' ' + response.statusText);
-            error.response = response;
-            throw error;
-        }
-    },
-    error => {
-        var errmsg = new Error(error.message);
-        throw errmsg;
-    })
-    dispatch({
+  await axios
+    .get("http://localhost:8080/api/doctors/all")
+    .then((res) => {
+      dispatch({
         type: GET_DOCTORS,
         payload: res.data,
+      });
     })
-    .catch(error => dispatch(getDoctorError(error.message)));
+    .then(
+      (response) => {
+        if (response.status == "ok") {
+          return response;
+        } else {
+          var error = new Error(
+            "Error" + response.status + " " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+    },
+      (error) => {
+        var errmsg = new Error(error.message);
+        throw errmsg;
+      }
+    )
+    .catch((error) => dispatch(getDoctorError(error.message)));
 };
 
 export const getDoctorError = (errmsg) => ({
-    type: GET_ERRORS,
-    payload: errmsg
-})
+  type: GET_ERRORS,
+  payload: errmsg,
+});
 
 export const deleteDoctor = (id) => async (dispatch) => {
-    if(window.confirm("Are you sure ? This will delete the Doctor details")) {
-        const res = await axios.delete(`http://localhost:8080/api/doctors/delete/${id}`);
-        console.log(res);
-        dispatch({
-            type:DELETE_DOCTOR,
-            payload:id
-        });
-    }
+  if (window.confirm("Are you sure ? This will delete the Doctor details")) {
+    const res = await axios.delete(
+      `http://localhost:8080/api/doctors/delete/${id}`
+    );
+    console.log(res);
+    dispatch({
+      type: DELETE_DOCTOR,
+      payload: id,
+    });
+  }
 };
 
-
-    export const getDoctor=(id,history)=>async dispatch=>{
-        const res=await axios.get(`http://localhost:8080/api/doctors/${id}`);
-        dispatch({
-            type:GET_DOCTOR,
-            payload:res.data
-        });
-    };
+export const getDoctor = (id, history) => async (dispatch) => {
+  const res = await axios.get(`http://localhost:8080/api/doctors/${id}`);
+  dispatch({
+    type: GET_DOCTOR,
+    payload: res.data,
+  });
+};
--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -1,10 +1,15 @@
 import {GET_ERRORS} from '../actions/type';
 
-const initialState={};
+const initialState={
+    errorMessage: null
+};
 export default function(state=initialState,action) {
     switch(action.type) {
         case GET_ERRORS:
-            return action.payload;
+            return {
+                ...state,
+                errorMessage: action.payload
+            }
         default:
             return state;
     }
